92042052a36303c35be832c0f2cc1cc7af3dbea9
Jeff Oliver <jeffoliver3384@gmail.com>
2021-10-12 19:24:37 -0400

    WIP: Image file reader working and stores image in state

--- src/components/InputBox.tsx
+++ src/components/InputBox.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, useRef } from "react";
+import React, { FormEvent, ChangeEvent, useRef, useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import { collection, addDoc, serverTimestamp } from "firebase/firestore";
 import { db } from "../firebase.setup";
@@ -7,15 +7,18 @@ import { CameraIcon, VideoCameraIcon } from "@heroicons/react/solid";
 
 const InputBox: React.FC = () => {
   const { user } = useAuth(),
-    inputRef = useRef<HTMLInputElement>(null);
+    inputRef = useRef<HTMLInputElement>(null),
+    filePickerRef = useRef<HTMLInputElement>(null),
+    [imageToPost, setImageToPost] = useState<string | null>(null);
 
+  // Send Post
   const sendPost = async (e: FormEvent) => {
     e.preventDefault();
 
     if (!inputRef.current!.value) return;
 
     try {
-      const docRef = await addDoc(collection(db, "posts"), {
+      await addDoc(collection(db, "posts"), {
         message: inputRef.current!.value,
         name: user.displayName,
         email: user.email,
@@ -24,11 +27,26 @@ const InputBox: React.FC = () => {
       });
 
       inputRef.current!.value = "";
-
-      console.log(docRef);
     } catch (error) {
       console.log(error);
     }
+  };
+
+  // Add Image to Post
+  const addImageToPost = (e: ChangeEvent<HTMLInputElement>) => {
+    const reader = new FileReader();
+    if (e.target.files![0]) {
+      reader.readAsDataURL(e.target.files![0]);
+    }
+
+    reader.onload = (readerEvent) => {
+      setImageToPost(readerEvent.target!.result as string);
+    };
+  };
+
+  // Set image state back to null
+  const removeImage = () => {
+    setImageToPost(null);
   };
 
   return (
@@ -36,7 +54,7 @@ const InputBox: React.FC = () => {
       <div className='flex space-x-4 p-4 items-center'>
         <img
           className='rounded-full'
-          src={user.photoURL + "?type=large"}
+          src={user.photoURL}
           alt='profile'
           width={40}
           height={40}
@@ -54,6 +72,16 @@ const InputBox: React.FC = () => {
             Submit
           </button>
         </form>
+
+        {imageToPost && (
+          <div
+            onClick={removeImage}
+            className='flex flex-col filter hover:brightness-110 transition duration-150 transform hover:scale-105 cursor-pointer'
+          >
+            <img className='h-10 object-contain' src={imageToPost} alt='post' />
+            <p className='text-xs text-red-500 text-center'>Remove</p>
+          </div>
+        )}
       </div>
 
       <div className='flex justify-evenly p-3 border-t'>
@@ -61,9 +89,18 @@ const InputBox: React.FC = () => {
           <VideoCameraIcon className='h-7 text-red-500' />
           <p className='text-xs sm:text-sm xl:text-base'>Live Video</p>
         </div>
-        <div className='inputIcon'>
+        <div
+          className='inputIcon'
+          onClick={() => filePickerRef.current?.click()}
+        >
           <CameraIcon className='h-7 text-green-400' />
           <p className='text-xs sm:text-sm xl:text-base'>Photo/Video</p>
+          <input
+            onChange={addImageToPost}
+            ref={filePickerRef}
+            type='file'
+            hidden
+          />
         </div>
         <div className='inputIcon'>
           <EmojiHappyIcon className='h-7 text-yellow-300' />
