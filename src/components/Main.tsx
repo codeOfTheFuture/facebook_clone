a97aab6afa8763ed8b2c87b6b44fe764d67e0887
Jeff Oliver <jeffoliver3384@gmail.com>
2021-10-19 18:10:43 -0400

    WIP: Widgets component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Feed from "./Feed";
 import Sidebar from "./Sidebar";
+import Widgets from "./Widgets";
 
 const Main: React.FC = () => {
   return (
     <main className='flex' data-test='component-main'>
       <Sidebar />
       <Feed />
-      {/*  Widgets  */}
+      <Widgets />
     </main>
   );
 };
--- src/components/Widgets.tsx
+++ src/components/Widgets.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { SearchIcon } from "@heroicons/react/outline";
+import { DotsHorizontalIcon, VideoCameraIcon } from "@heroicons/react/solid";
+
+import { contacts } from "../data/widgetsData";
 
 const Widgets: React.FC = () => {
   return (
-    <div>
-      <h1>Widgets</h1>
+    <div className='hidden lg:flex flex-col w-60 p-2 mt-5'>
+      <div className='flex justify-between items-center text-gray-500 mb-5'>
+        <h2 className='text-xl'>Contacts</h2>
+        <div className='flex space-x-2'>
+          <VideoCameraIcon height={15} />
+          <SearchIcon height={15} />
+          <DotsHorizontalIcon height={15} />
+        </div>
+      </div>
     </div>
   );
 };
