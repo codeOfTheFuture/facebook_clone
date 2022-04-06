c9841da874d925e9cba798d50237c15081e2204f
Jeff Oliver <jeffoliver3384@gmail.com>
2022-04-06 01:21:08 -0400

    privacy policy page for facebook app to go live

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import { ProtectedRoute } from "./components/ProtectedRoute";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
 
 const App: React.FC = () => {
   return (
@@ -10,6 +11,7 @@ const App: React.FC = () => {
       <Switch>
         <ProtectedRoute exact path='/' component={Home} />
         <Route path='/login' render={() => <Login />} />
+        <Route path='/privacy' render={() => <PrivacyPolicy />}
       </Switch>
     </div>
   );
