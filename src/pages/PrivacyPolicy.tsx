be804c1aca9510471ab64d9fc26f1ce1de5ed0a1
Jeff Oliver <jeffoliver3384@gmail.com>
2022-04-06 01:18:22 -0400

    privacy policy

---
+++ src/pages/PrivacyPolicy.tsx
@@ -0,0 +1,233 @@
+import React from 'react'
+
+const PrivacyPolicy = () => {
+  return (
+    <div>
+      <p>
+        Privacy Policy
+        Welcome to our/my Facebook page!
+
+        The agreement on joint responsibility according to GDPR Art. 26 can be found at https://
+        www.facebook.com/legal/terms/page controller addendum.
+        This agreement was provided in this form by Facebook Ireland Ltd. and we/I as user/s of this
+        Facebook pages accept it by using Facebook.
+        We/I cannot assess whether this agreement meets the requirements of the GRDPR Art. 26 in a
+        sufficient manner.
+        Information about our/my Facebook page
+        We/I operate this page to draw attention to our/my services/products and to contact you as a
+        visitor and user of this Facebook page and our/my website(s). You can find further information
+        about us/me and our/my activities, company etc at https://…..
+        As the operator of the Facebook page, we are/I am not interested in the collection and further
+        processing of your individual personal data for analysis or marketing purposes. Further
+        information on how we/I handle personal data can be found in our/my privacy policy on our/my
+        website at [enter the link to your privacy policy on your own website here].
+        The operation of this Facebook page, including the processing of a user's personal data, is based
+        on our/my legitimate interests in a modern and supportive mean to inform and interact with our/
+        my users and visitors in accordance with GDPR Art. 6 para. 1 lit. (f).
+        © RAin Sabrina Keese-Haufs | lawlikes | as of December 13, 2018 | Translated into EN by Tanya Quintieri | Mrs. Divi
+        This document serves as general information and does not constitute legal advice in individual cases. Before applying the information provided, individual
+        adjustments are necessary; the passages marked in yellow must always be adjusted. In addition, before publication you have to ensure that all links are
+        fully functional. Furthermore, no guarantees and no liability for topicality and completeness can be accepted.
+        Reproduction is permitted free of charge using the copyright identification.
+        lawlikes
+        LEGALESE simplified
+        Consent according to GDPR Art. 6 Para. 1 S.1 lit. (b) for the processing of personal data can only
+        be technically obtained by Facebook. Currently, Facebook does not offer such a consent option.
+        Data processing through Facebook
+        In its ruling on June 5, 2018, the European Court of Justice (https://curia.europa.eu/jcms/upload/
+        docs/application/pdf/2018-06/cp180081en.pdf) decided that the operator of a Facebook page is
+        responsible for the processing of personal data together with Facebook.
+        We are/I am aware that Facebook processes user data for the following purposes:
+        • Advertising (analysis, provision of personalized advertising),
+        • creation of user profiles, and
+        • market research.
+        Facebook uses cookies to store and further process this information, i.e. small text files that are
+        stored on the user's various end devices. If the user has a Facebook profile and is logged in to it,
+        it is also stored and analyzed across devices.
+        Facebook's privacy policy contains further information on data processing: https://
+        www.facebook.com/about/privacy.
+        It is possible to opt out here: https://www.facebook.com/settings?tab=ads and here http://
+        www.youronlinechoices.com.
+        Facebook Inc., the US parent company of Facebook Ireland Ltd., is certified under the EU-U.S.
+        Privacy Shield and thus promises to comply with European data protection guidelines. For more
+        information on Facebook's Privacy Shield status, click here: https://www.privacyshield.gov/
+        participant?id=a2zt0000000GnywAAC&status=Active.
+        The transfer and further processing of users' personal data in third countries, e. g. the USA, as
+        well as the associated possible risks for users cannot be excluded by us/me as the operator of
+        this page.
+        Statistical data
+        Facebook's so-called "Insights" provides statistical data of different categories for this page and
+        these statistics can be accessed by us/me. They are generated and provided by Facebook. As
+        the operator of this page, we/I have no influence on their generation and presentation. We/I
+        cannot disable this function or prevent the generation and processing of data. It is possible to
+        select a certain time range to display statistics for the categories of likes, subscribers, reached
+        persons and interacting persons to receive the following data from Facebook in relation to our/my
+        Facebook page:
+        Total number of page views, "Likes", page activity, post interactions, reach, video views, post
+        reach, comments, shared content, answers, proportion of men and women, origin by country and
+        city, language, visits to and clicks in the shop, clicks on route planners, clicks on telephone
+        © RAin Sabrina Keese-Haufs | lawlikes | as of December 13, 2018 | Translated into EN by Tanya Quintieri | Mrs. Divi
+        This document serves as general information and does not constitute legal advice in individual cases. Before applying the information provided, individual
+        adjustments are necessary; the passages marked in yellow must always be adjusted. In addition, before publication you have to ensure that all links are
+        fully functional. Furthermore, no guarantees and no liability for topicality and completeness can be accepted.
+        Reproduction is permitted free of charge using the copyright identification.
+        lawlikes
+        LEGALESE simplified
+        numbers. In the same manner, Facebook provides information about Facebook groups connected
+        to our/my Facebook page.
+        Due to the constant development of Facebook, the availability and processing of the data
+        changes all the time, so that we/I once again refer to the link to Facebook's privacy policy already
+        mentioned above for further details.
+        We/I use this aggregated data to make our/my contributions and activities on our/my Facebook
+        page more attractive to users. For example, we/I use the ratios regarding age and gender to
+        appropriately address our/my visitors, and stats on when the most interactions take place to post
+        at the most suitable time. Information about the type of devices used by visitors helps us/me to
+        create the most suitable visuals. In accordance with the Facebook ToU, which each user has
+        agreed to when creating their Facebook profile, we/I may identify the subscribers and likers of the
+        page and view their profiles and other shared information about them.
+        User rights
+        If you have any questions about your rights, please contact Facebook directly.
+        You have a right to free information about your personal data stored on Facebook and, if
+        necessary, a right to correction, restriction of processing or deletion of this data. You also have
+        the right to data transfer. Finally, you also have the right to complain about Facebook's processing
+        of your personal data to the data protection supervisory authority.
+        If information questions are submitted to us/me as the site operator, the additional agreement with
+        Facebook obliges us to forward these questions — whether from private individuals or authorities
+        — to Facebook within 7 days.
+        This is also a result of the above-mentioned Controller Addendum: https://www.facebook.com/
+        legal/terms/page controller addendum.
+        If you need support or have any other questions, please contact us/me by email at this email
+        address. If you no longer wish to have your data processed in the future as described here, please
+        cancel the connection of your user profile to my/our page by using the function "Unlike this
+        Page".
+        Information as of December 13, 2018
+        © 2018 Lawyer Sabrina Keese-Haufs
+        Optional: www.lawlikes.de
+        © RAin Sabrina Keese-Haufs | lawlikes | as of December 13, 2018 | Translated into EN by Tanya Quintieri | Mrs. Divi
+        This document serves as general information and does not constitute legal advice in individual cases. Before applying the information provided, individual
+        adjustments are necessary; the passages marked in yellow must always be adjusted. In addition, before publication you have to ensure that all links are
+        fully functional. Furthermore, no guarantees and no liability for topicality and completeness can be accepted.
+        Reproduction is permitted free of charge using the copyright identification.
+        lawlikes
+        LEGALESE simplified
+        Sample privacy policy for a Facebook group
+        Privacy Policy
+        Welcome to our/my Facebook group!
+        Name and address of the parties responsible:
+        Entities jointly responsible for the operation of this Facebook group in the sense of the GDPR as
+        well as other data protection regulations:
+        Facebook Ireland Ltd. (hereinafter "Facebook")
+        4 Grand Canal Square
+        Grand Canal Harbour
+        Dublin 2
+        Ireland
+        and
+        Company or name of entrepreneur
+        Address
+        Postcode and city
+        Country
+        The agreement on joint responsibility according to GDPR Art. 26 can be found at https://
+        www.facebook.com/legal/terms/page controller addendum.
+        This agreement was provided in this form by Facebook Ireland Ltd. and we/I as user/s of this
+        Facebook group accept it by using Facebook.
+        We/I cannot assess whether this agreement meets the requirements of the GRDPR Art. 26 in a
+        sufficient manner.
+        Information about our/my Facebook group
+        We/I operate this group to draw attention to our/my services/products and to contact you as a
+        member and user of this Facebook group as well as our/my Facebook page and our/my
+        website(s). You can find further information about us/me and our/my activities, company etc. at
+        https://....
+
+        As the operator of this Facebook group, I am not interested in the collection and further
+        processing of your individual personal data for analysis or marketing purposes. Further
+        information on how we/I handle personal data can be found in our/my privacy policy on our/my
+        website at [enter the link to your privacy policy on your own website here].
+        © RAin Sabrina Keese-Haufs | lawlikes | as of December 13, 2018 | Translated into EN by Tanya Quintieri | Mrs. Divi
+        This document serves as general information and does not constitute legal advice in individual cases. Before applying the information provided, individual
+        adjustments are necessary; the passages marked in yellow must always be adjusted. In addition, before publication you have to ensure that all links are
+        fully functional. Furthermore, no guarantees and no liability for topicality and completeness can be accepted.
+        Reproduction is permitted free of charge using the copyright identification.
+        lawlikes
+        LEGALESE simplified
+        The operation of this Facebook group, including the processing of a user's personal data, is
+        based on our/my legitimate interests in a modern and supportive mean to inform and interact with
+        our/my users and visitors in accordance with GDPR Art. 6 para.1 lit. (f).
+        Consent according to GDPR Art. 6 Para. 1 S.1 lit. (b) GDPR for the processing of personal data
+        can only be technically obtained by Facebook. Currently, Facebook does not offer such a consent
+        option.
+        Data processing through Facebook
+        In its ruling on June 5, 2018, the European Court of Justice (https://curia.europa.eu/jcms/upload/
+        docs/application/pdf/2018-06/cp180081en.pdf) decided that the operator of a Facebook group is
+        responsible for the processing of personal data together with Facebook.
+        We are/I am aware that Facebook processes user data for the following purposes:
+        • Advertising (analysis, provision of personalized advertising),
+        • creation of user profiles, and
+        • market research.
+        Facebook uses cookies to store and further process this information, i.e. small text files that are
+        stored on the user's various end devices. If the user has a Facebook profile and is logged in to it,
+        it is also stored and analyzed across devices.
+        Facebook's privacy policy contains further information on data processing: https://
+        www.facebook.com/about/privacy.
+        It is possible to opt out here: https://www.facebook.com/settings?tab=ads and here http://
+        www.youronlinechoices.com.
+        Facebook Inc., the US parent company of Facebook Ireland Ltd., is certified under the EU-U.S.
+        Privacy Shield and thus promises to comply with European data protection guidelines. For more
+        information on Facebook's Privacy Shield status, click here: https://www.privacyshield.gov/
+        participant?id=a2zt0000000GnywAAC&status=Active.
+        The transfer and further processing of users' personal data in third countries, e. g. the USA, as
+        well as the associated possible risks for users cannot be excluded by us/me as the operator of
+        this group.
+        Statistical data
+        Facebook's so-called “Group Insights" provides statistical data of different categories for this
+        group and these statistics can be accessed by us/me. They are generated and provided by
+        Facebook. As the operator of this group, we/I have no influence on their generation and
+        presentation. We/I cannot disable this function or prevent the generation and processing of data.
+        It is possible to select a certain time range to display statistics for the categories of members,
+        reached persons and interacting persons to receive the following data from Facebook in relation
+        to our/my Facebook group:
+        © RAin Sabrina Keese-Haufs | lawlikes | as of December 13, 2018 | Translated into EN by Tanya Quintieri | Mrs. Divi
+        This document serves as general information and does not constitute legal advice in individual cases. Before applying the information provided, individual
+        adjustments are necessary; the passages marked in yellow must always be adjusted. In addition, before publication you have to ensure that all links are
+        fully functional. Furthermore, no guarantees and no liability for topicality and completeness can be accepted.
+        Reproduction is permitted free of charge using the copyright identification.
+        lawlikes
+        LEGALESE simplified
+        Total number of members, number of new members, name and profile information of each
+        member and those requesting to join, number of posts and other interactions, popular days and
+        times for interactions, number of membership requests, number of active members, most active
+        contributing members, most popular contributions, distribution of members by gender and age,
+        and by country and location.
+        Due to the constant development of Facebook, the availability and processing of the data
+        changes all the time, so that we/I once again refer to the link to Facebook's privacy policy already
+        mentioned above for further details.
+        We/I use this aggregated data to make our/my contributions and activities in our/my Facebook
+        group more attractive to users. For example, we/I use the ratios regarding age and gender to
+        appropriately address our/my visitors, and stats on when the most interactions take place to post
+        at the most suitable time. Information about the type of devices used by visitors helps us/me to
+        create the most suitable visuals. In accordance with the Facebook ToU, which each user has
+        agreed to when creating their Facebook profile, we/I may identify the members of this group and
+        view their profiles and other shared information about them. I/we use the information about the
+        most active participants in the group to address them in a targeted way on the topics dealt with in
+        the group.
+        User rights
+        If you have any questions about your rights, please contact Facebook directly.
+        You have a right to free information about your personal data stored on Facebook and, if
+        necessary, a right to correction, restriction of processing or deletion of this data. You also have
+        the right to data transfer. Finally, you also have the right to complain about Facebook's processing
+        of your personal data to the data protection supervisory authority.
+        If information questions are submitted to us/me as the group operator, the additional agreement
+        with Facebook obliges us to forward these questions — whether from private individuals or
+        authorities — to Facebook within 7 days.
+        This is also a result of the above-mentioned Controller Addendum: https://www.facebook.com/
+        legal/terms/page controller addendum.
+        If you need support or have any other questions, please contact us/me by email at this email
+        address. If you no longer wish to have your data processed in the future as described here, please
+        leave the group.
+        Information as of December 13, 2018
+        © 2018 Lawyer Sabrina Keese-Haufs
+      </p>
+    </div>
+  )
+}
+
+export default PrivacyPolicy;
